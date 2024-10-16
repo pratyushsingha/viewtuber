6f3a34be4fd5266c4bf17ed9caeff9ecad12e20c
Pratyush Singha <pratyushsingha83@gmail.com>
2024-10-17 00:47:07 +0530

    first commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { DatePicker } from "antd";
+
+export default function Web() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <DatePicker />
+    </>
+  );
+}
